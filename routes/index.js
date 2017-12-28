2ddc4532fd51833d9f16f87e309472219370caa8
Blaine Richardson <blainerichardson@Blaines-MacBook-Pro.local>
2017-12-28 00:38:08 -0600

    Commit #4 { Added the first simple marko views for GET routes }

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,15 @@
 const Router = require('koa-router');
 const router = new Router();
 
-const marko = require('koa-marko');
-const template = require('./template.marko');
 // ** Query declerations **//
 const queries = require('../db/queries');
 
 // ** Table declerations ** //
 const tables = require('../db/tables');
 
-router.use(marko());
-
 router.get('/', async (ctx) => {
-  try {
-    let selectReference = '*';
-
-    const dataCustomers = await queries.objects.getAllObjects(
-      tables.customers.customersTable,
-      selectReference
-    );
-
-    ctx.render(template, {customer: dataCustomers}) ;
-  } catch (err) {
-    console.log(err)
-  }
-})
+    ctx.body = {
+      status: 'success'
+    };
+  })
 module.exports = router;
